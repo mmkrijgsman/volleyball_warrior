e684b3d48b9ab84b200ec80cb7cadb72b8f9f3c0
mmkrijgsman <m3krijgsman@gmail.com>
2025-12-26 23:31:54 +0100

    Delete src directory

--- src/VolleyballTracker.jsx
+++
@@ -1,8 +0,0 @@
-export default function VolleyballTracker() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>🏐 Volleyball Match Tracker</h1>
-      <p>De site draait correct 🎉</p>
-    </div>
-  );
-}
--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import VolleyballTracker from './VolleyballTracker'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <VolleyballTracker />
-  </React.StrictMode>
-)
